ec1220253926624c00c7436db391235b65b43182
Marlon Florez <116696754+mallonflowerz@users.noreply.github.com>
2026-07-19 15:07:04 -0500

    Configurando enviroments para prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBXzuTHzr9_r_yAcn0FGzBQScQPGXJn3x8",
+    authDomain: "todolist-66f0a.firebaseapp.com",
+    projectId: "todolist-66f0a",
+    storageBucket: "todolist-66f0a.firebasestorage.app",
+    messagingSenderId: "705208179009",
+    appId: "1:705208179009:web:6b1294a65b9fcbc3a9a9c0"
+  }
 };
